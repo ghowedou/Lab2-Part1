2dd12dfa09558aacb93ccdd2ac25493b51bb4a88
ghowedou <gilliandoucet@gmail.com>
2020-09-22 14:55:53 +0000

    Honestly just messing aroud trying to make things work

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 user_in = input('Please enter a physical constant:')
 
-if user_in == 'speed of light':
-    print('2.9979245e8')
-if user_in == 'Plancks Constant':
-    print('6.62606896e−34')      
-if user_in == 'Boltzmanns Constant':
-    print('1.3806504e−23')
-if user_in == 'Electon Mass':
-    print('9.10938215e−31')
-if user_in == 'Atomic Mass Unit':
-    print('1.660538782e−27')    
-if user_in == 'Electron Charge':
-    print('1.602176487e−19')
-if user_in == 'Bohr Radius':
-    print('5.2917720859e−11')
-if user_in == 'Gravitational Constant':
-    print('6.67428e−11')
-if user_in == 'Stefan-Boltzmann Constant':
-    print('5.670400e−8')
+def physconst(user_in):
+    if user_in == 'speed of light':
+        c=2.9979245e8
+        print(c)
+        CC= physconst(c)
+        Return
+    if user_in == 'Plancks Constant':
+        print('6.62606896e−34')      
+
+    if user_in == 'Boltzmanns Constant':
+        print('1.3806504e−23')
+
+    if user_in == 'Electon Mass':
+        print('9.10938215e−31')
+
+    if user_in == 'Atomic Mass Unit':
+        print('1.660538782e−27')   
+
+    if user_in == 'Electron Charge':
+        print('1.602176487e−19')
+
+    if user_in == 'Bohr Radius':
+        print('5.2917720859e−11')
+
+    if user_in == 'Gravitational Constant':
+        print('6.67428e−11')
+
+    if user_in == 'Stefan-Boltzmann Constant':
+        print('2.9979245e8')
