96a0f2f6902c536398b79318adf85f7a1d1fc53d
ghowedou <gilliandoucet@gmail.com>
2020-09-22 14:29:25 +0000

    Trying to make function

---
+++ main.py
@@ -0,0 +1,11 @@
+user_in = input('Please enter a physical constant:')
+
+def physconst(user_in):
+
+    if user_in == 'speed of light':
+        print('You have inputed the speed of light, symbol "c", which has a value of 2.9979245e8 and units of m s-1')
+        cc = physconst('speed of light')
+    
+    
+
+
