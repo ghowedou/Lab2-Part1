63837ba3c00568c8462fa85c326d2f46d2604d06
ghowedou <gilliandoucet@gmail.com>
2020-09-22 14:45:22 +0000

    Added all variabless

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 user_in = input('Please enter a physical constant:')
 
-def physconst(user_in):
-
-    if user_in == 'speed of light':
-        print('You have inputed the speed of light, symbol "c", which has a value of 2.9979245e8 and units of m s-1')
-        cc = physconst('speed of light')
-    
-    
-
-
+if user_in == 'speed of light':
+    print('2.9979245e8')
+if user_in == 'Plancks Constant':
+    print('6.62606896e−34')      
+if user_in == 'Boltzmanns Constant':
+    print('1.3806504e−23')
+if user_in == 'Electon Mass':
+    print('9.10938215e−31')
+if user_in == 'Atomic Mass Unit':
+    print('1.660538782e−27')    
+if user_in == 'Electron Charge':
+    print('1.602176487e−19')
+if user_in == 'Bohr Radius':
+    print('5.2917720859e−11')
+if user_in == 'Gravitational Constant':
+    print('6.67428e−11')
+if user_in == 'Stefan-Boltzmann Constant':
+    print('5.670400e−8')
